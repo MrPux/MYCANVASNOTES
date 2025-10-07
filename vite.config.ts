522e67daf042b72f8ca4747630bf3fccb71dddbb
mrpux <mrpuxdev@gmail.com>
2025-10-07 18:06:23 -0400

    fix(vite): set base path to root for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/MYCANVASNOTES/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
